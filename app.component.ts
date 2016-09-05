169c54b306d427ff4d827eb0fcc08453bc44fb0b
Liu Cui <c.liu@piala.co.jp>
2016-09-05 16:34:42 +0900

    init

---
+++ app.component.ts
@@ -0,0 +1,28 @@
+import './app.loader.ts';
+import {Component, ViewEncapsulation} from "@angular/core";
+import {AppState} from "./app.state";
+import {BaThemeConfigProvider, BaThemeConfig} from "./theme";
+import {BaThemeRun} from "./theme/directives";
+import {BaImageLoaderService, BaThemePreloader, BaThemeSpinner} from "./theme/services";
+import {layoutPaths} from "./theme/theme.constants";
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'ng2-blur-theme',
+  directives: [BaThemeRun],
+  providers: [BaThemeConfigProvider, BaThemeConfig, BaImageLoaderService, BaThemeSpinner],
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./ng2-blur-theme.scss'],
+  template: `
+    <main baThemeRun>
+      <div class="additional-bg"></div>
+      <ng-content></ng-content>
+    </main>
+  `
+})
+export class ng2BlurThemeComponent { 
+
+}
